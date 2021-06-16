b2beb89ce9976b2301e728566fc4465b328aa4eb
Ricardo Antônio Mazzuco <ricardopop96@gmail.com>
2021-06-16 19:29:07 -0300

    refazendo o empastamento do projeto

---
+++ src/main/webapp/js/controllers/newFilmCtrl.js
@@ -0,0 +1,8 @@
+angular.module("filmorum").controller("newFilmCtrl", function($scope, $http) {
+    $scope.app = "Filmorum";
+    $scope.film = {};
+
+    $scope.newFilm = function(film){
+        $http.post("http://localhost:8080/filmorum/api/film", film);
+    };
+});
