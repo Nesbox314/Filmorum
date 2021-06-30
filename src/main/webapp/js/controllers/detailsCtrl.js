63df9413b63bc3d8e660ca7e1d740092e51290e4
Ricardo Antônio Mazzuco <ricardopop96@gmail.com>
2021-06-30 18:36:17 -0300

    sistema de cadastro e visualização de reviews pronto

--- src/main/webapp/js/controllers/detailsCtrl.js
+++ src/main/webapp/js/controllers/detailsCtrl.js
@@ -1,6 +1,7 @@
 angular.module("filmorum").controller("detailsCtrl", function($scope, $http, $routeParams) {
     $scope.film = {};
     $scope.reviews = {};
+    $scope.numberOfReviews = 0;
     $scope.filmId = $routeParams.id;
 
     $http.get("http://localhost:8080/filmorum/api/film/" + $scope.filmId).then(function (response){
@@ -9,6 +10,7 @@ angular.module("filmorum").controller("detailsCtrl", function($scope, $http, $ro
 
     $http.get("http://localhost:8080/filmorum/api/analysis/film/" + $scope.filmId).then(function (response){
         $scope.reviews = response.data;
+        $scope.numberOfReviews = response.data.length;
     });
 
 });
--- src/main/webapp/js/controllers/reviewCtrl.js
+++ src/main/webapp/js/controllers/reviewCtrl.js
@@ -3,17 +3,14 @@ angular.module("filmorum").controller("reviewCtrl", function($scope, $http, $win
 
     let token = localStorage.getItem("token");
     if(token){
-        let decryptedToken = atob(token);
-        $scope.review.userId = decryptedToken.id;
+        $scope.review.userId = JSON.parse(atob(token)).id;
     }
     $scope.review.movie = $routeParams.id;
-
-    console.log($routeParams.id);
 
     $scope.newReview = function(review){
         $http.post("http://localhost:8080/filmorum/api/analysis/", review).then(function (response){
             if(response.status == 200){
-                $window.location.href = 'index.html';
+                $window.location.href = '/filmorum/#!/details/' + $routeParams.id;
             }
         });
     }
