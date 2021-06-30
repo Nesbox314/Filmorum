84ef0137e238dbca92f0c2c9863849812dadf142
Ricardo Antônio Mazzuco <ricardopop96@gmail.com>
2021-06-30 18:58:58 -0300

    média de reviews pronta

--- src/main/webapp/js/controllers/detailsCtrl.js
+++ src/main/webapp/js/controllers/detailsCtrl.js
@@ -2,6 +2,7 @@ angular.module("filmorum").controller("detailsCtrl", function($scope, $http, $ro
     $scope.film = {};
     $scope.reviews = {};
     $scope.numberOfReviews = 0;
+    $scope.avgOfReviews = 0;
     $scope.filmId = $routeParams.id;
 
     $http.get("http://localhost:8080/filmorum/api/film/" + $scope.filmId).then(function (response){
@@ -11,6 +12,11 @@ angular.module("filmorum").controller("detailsCtrl", function($scope, $http, $ro
     $http.get("http://localhost:8080/filmorum/api/analysis/film/" + $scope.filmId).then(function (response){
         $scope.reviews = response.data;
         $scope.numberOfReviews = response.data.length;
+        $scope.reviews.forEach(avg);
     });
+
+    function avg(review){
+        $scope.avgOfReviews = $scope.avgOfReviews + review.avaliation;
+    }
 
 });
